89748a2552b511ba114b931aa36876e44bfee2ac
유병우 <conner903@naver.com>
2023-07-31 22:00:17 +0900

    🚧 🎉 : 프로젝트 생성 및 vite module federation진행중
      - remoteApp can't found type definition error

---
+++ packages/react-vite/remote/src/components/Header.tsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <header>
+      <div
+        style={{
+          display: "flex",
+          paddingInline: "8px",
+          alignItems: "center",
+          background: "#0e0e0e",
+          color: "#e0e0e0",
+          width: "100%",
+          height: "48px",
+        }}
+      >
+        헤더 컴포넌트
+      </div>
+    </header>
+  );
+};
+
+export default Header;
